386fa0695b2569ebf4a28f5bda8528da73723f54
iyalice <107679775+iyalice@users.noreply.github.com>
2024-03-16 11:37:51 +0800

    Add files via upload

---
+++ init.R
@@ -0,0 +1,22 @@
+library(SPARK)
+library(R.matlab)
+library(matlabr)
+load("E:/R/R-4.1.2/library/SPARK/data/Layer2_BC_Count.rds")
+rawcount[1:10,1:10]
+info <- cbind.data.frame(x=as.numeric(sapply(strsplit(colnames(rawcount),split="x"),"[",1)),
+                         y=as.numeric(sapply(strsplit(colnames(rawcount),split="x"),"[",2)),
+                         total_counts=apply(rawcount,2,sum))
+rownames(info) <- colnames(rawcount)
+spark <- CreateSPARKObject(counts=rawcount, 
+                           location=info[,1:2],
+                           percentage = 0.1, 
+                           min_total_counts = 10)
+spark@lib_size <- apply(spark@counts, 2, sum)
+spark <- spark.vc(spark, 
+                  covariates = NULL, 
+                  lib_size = spark@lib_size, 
+                  num_core = 5,
+                  verbose = F)
+N=250
+
+
---
+++ optimizer.m
@@ -0,0 +1,34 @@
+load GAPDH.mat
+X = double([X.x, X.y]);
+y = double(y);
+
+%% Load GPML
+addpath(genpath('大创\gpml'))
+
+%% Set up model.
+meanfunc = {@meanZero};
+hyp.mean = [];
+
+covfunc1 = {@covSum,{{@covMask,{[1 0],@covSEiso}},{@covMask,{[0 1],@covSEiso}}}}; 
+hyp.cov = [0 0 0 0];
+
+likfunc = @likGauss;
+hyp.lik = log(var(y)/10);
+
+%% Repeat...
+[hyp_opt, nlls] = minimize(hyp, @gp, -int32(100 * 3 / 3), @infExact, meanfunc, covfunc1, likfunc, X, y);
+best_nll = nlls(end);
+
+fid1 = fopen('F:/documents/大创/GAPDH_1.in','w');
+fprintf(fid1,'exp(-(X[i,1]-X[j,1])^2/(2*exp(%f)^2))*exp(%f)^2+exp(-(X[i,2]-X[j,2])^2/(2*exp(%f)^2))*exp(%f)^2',hyp_opt.cov);
+
+covfunc2 = {@covProd,{{@covMask,{[1 0],@covSEiso}},{@covMask,{[0 1],@covSEiso}}}}; 
+hyp2.mean = [];
+hyp2.cov = [0 0 0 0];
+hyp2.lik = log(var(y)/10);
+[hyp2_opt, nlls] = minimize(hyp2, @gp, -int32(100 * 3 / 3), @infExact, meanfunc, covfunc2, likfunc, X, y);
+best_nll2 = nlls(end);
+
+
+fid2 = fopen('F:/documents/大创/GAPDH_2.in','w');
+fprintf(fid2,'exp(-(X[i,1]-X[j,1])^2/(2*exp(%f)^2))*exp(%f)^2*exp(-(X[i,2]-X[j,2])^2/(2*exp(%f)^2))*exp(%f)^2',hyp2_opt.cov);
